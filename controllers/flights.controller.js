cd6e6d1c38e74a1de480421bfe53f51ef57ea48f
Cody Liu <cliu@skillstorm.com>
2022-06-23 13:07:41 -0400

    demo 1

--- controllers/flights.controller.js
+++ controllers/flights.controller.js
@@ -1,22 +1,35 @@
-// import Movie model
+// import Flight model
 const Flight = require('../models/Flight.model.js');
 
-const createFlight = async ({flightNumber, departure, arrival, departureAirport, arrivalAirport, passengerLimit, currNumPassengers}) => {
-    try {
+const createFlight = async (req) => {
+    flightNumber = req["flight-number"];
+    departure = req['departure-time'];
+    arrival = req['arrival-time'];
+    departureAirport = req['departure-airport'];
+    arrivalAirport = req['arrival-airport'];
+    passengerLimit = req['flight-capacity'];
+    currNumPassengers = req['flight-passengers'];
 
+    try {
         // if arrivalDate before departureDate or arrivalTime @/before departureTime, throw an error
         if (departure >= arrival) {
             throw "Departure Time must come before Arrival Time.";
         }
 
-        // if currNumPassengers > passengerLimit, throw an error
-        if (currNumPassengers > passengerLimit) {
-            throw "Too many passengers on this flight, not enough seats.";
-        }
-
         // arrival airport must be different from departureAirport
         if (departureAirport === arrivalAirport) {
             throw "Arrival Airport must be different from Departure Airport";
+        }
+
+        // if currNumPassengers > passengerLimit, throw an error
+        if (parseInt(currNumPassengers) > parseInt(passengerLimit)) {
+            throw "Too many passengers on this flight, not enough seats.";
+        }
+
+        const duplicateFlight = await Flight.findOne({flightNumber});
+
+        if (duplicateFlight) {
+            throw "This flight number is already in use. Cannot have duplicates."
         }
 
         const flight = new Flight({
@@ -28,6 +41,7 @@ const createFlight = async ({flightNumber, departure, arrival, departureAirport,
             passengerLimit,
             currNumPassengers
         });
+
         await flight.save();
 
         return flight._id;
@@ -38,9 +52,78 @@ const createFlight = async ({flightNumber, departure, arrival, departureAirport,
     }
 }
 
-const findAllFlights = async(limit = 0) => {
-    const flights = await Flight.find(); // get all flights
-    return flights;
+const findOneFlight = async(flightNumber) => {
+    const flight = await Flight.find({flightNumber});
+    return flight;
 }
 
-module.exports = { createFlight, findAllFlights};
+
+const findAllFlights = async() => {
+    const flights = await Flight.find(); // get all flights
+    return flights;
+    // TODO: implement filtering functionality
+    // {flightNumber: { $in: ids}}
+}
+
+const deleteFlight = async(flightNumber) => {
+    const flights = await Flight.deleteOne({flightNumber});
+    return flights;
+
+}
+
+const updateFlight = async (originalFlightNum, req) => {
+    flightNumber = req["flight-number"];
+    departure = req['departure-time'];
+    arrival = req['arrival-time'];
+    departureAirport = req['departure-airport'];
+    arrivalAirport = req['arrival-airport'];
+    passengerLimit = req['flight-capacity'];
+    currNumPassengers = req['flight-passengers'];
+
+    update = {
+        flightNumber: req["flight-number"],
+        departure: req['departure-time'],
+        arrival: req['arrival-time'],
+        departureAirport: req['departure-airport'],
+        arrivalAirport: req['arrival-airport'],
+        passengerLimit: req['flight-capacity'],
+        currNumPassengers: req['flight-passengers']
+    }
+
+    try {
+        // if arrivalDate before departureDate or arrivalTime @/before departureTime, throw an error
+        if (departure >= arrival) {
+            throw "Departure Time must come before Arrival Time.";
+        }
+
+        // arrival airport must be different from departureAirport
+        if (departureAirport === arrivalAirport) {
+            throw "Arrival Airport must be different from Departure Airport";
+        }
+
+        // if currNumPassengers > passengerLimit, throw an error
+        if (parseInt(currNumPassengers) > parseInt(passengerLimit)) {
+            throw "Too many passengers on this flight, not enough seats.";
+        }
+
+        const original = { flightNumber: String(originalFlightNum)};
+        const opts = { new: true };
+
+        Flight.findOneAndUpdate({flightNumber: {$eq:String(originalFlightNum)} },
+            update, opts, function (err, docs) {
+            if (err){
+                throw(err);
+            }
+            else{
+                return docs;
+            }
+        });
+
+    }
+
+    catch (err) {
+        throw { status: 400, message: err};
+    }
+}
+
+module.exports = { createFlight, findAllFlights, deleteFlight, findOneFlight, updateFlight};
--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const cors = require('cors');
-
-
+const { createFlight, findAllFlights, deleteFlight, findOneFlight, updateFlight } = require('./controllers/flights.controller');
 const app = express();
 const PORT = process.env.PORT || 8080; // default to 8080
 const Flight = require('./models/Flight.model.js');
+const ejs = require('ejs');
+
 
 // connect to database
 const connectToDB = async () => {
@@ -25,6 +26,7 @@ const connectToDB = async () => {
 connectToDB();
 
 // start app
+app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
@@ -34,43 +36,82 @@ app.listen(PORT, () => {
 
 // establish routes
 app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    res.render('index');
 })
 
-app.get('/flights/:id', (req, res) => {
-    res.send(`GET flight by ${req.params.id}`);
-})
-
+// just do simple delete and put. modify get to be a search functionality sort of thing
 app.get('/flights', async (req, res) => {
-    const movies = await findAllFlights();
-    res.json(movies);
+    try {
+        const flights = await findAllFlights();
+        res.render('flights', {
+            flightsList: flights
+        });
+    } catch (err) {
+        console.log(err);
+        // if request fails...return 5OO
+        res.status(err?.status || 500).json(err);
+    }
 });
 
-app.post('/flights', (req, res) => {
-    console.log(req.body);
-    /*
+
+app.post('/flights', async (req, res) => {
     try {
         // req contains post request data. createFlight deconstructs body so you can just pass in req.body here
         console.log(req.body)
         const flightId = await createFlight(req.body);
-        res.status(201).json({_id: flightId}); // 201 indicates successful resource creation
+        res.status(201);
 
     } catch (err) {
         console.log(err);
         // if request fails...return 5OO
         res.status(err?.status || 500).json(err);
     }
-    */
-   res.redirect(201, '/');
+
 });
 
-app.delete('/flights/:id', (req, res) => {
-    res.send(`DELETE flight with ${req.params.id}`);
+
+
+app.post('/delete/:id', async (req, res) => {
+    try {
+        const deleted = await deleteFlight(req.params.id);
+        const flights = await findAllFlights();
+        res.render('flights', {
+            flightsList: flights
+        });
+    } catch (err) {
+        console.log(err);
+        // if request fails...return 5OO
+        res.status(err?.status || 500).json(err);
+    }
+
 });
 
-app.put('/flights/:id', (req, res) => {
-    res.send(`PUT flight with ${req.params.id}`);
+app.get('/updates/:id', async (req, res) => {
+    try {
+        const flight = await findOneFlight(req.params.id);
+        console.log(flight);
+        res.render('updates', {
+            flightData: flight[0]
+        });
+    } catch (err) {
+        console.log(err);
+        // if request fails...return 5OO
+        res.status(err?.status || 500).json(err);
+    }
+
 });
+
+app.post('/updates/:id/edit', async (req, res) => {
+    try {
+        const newFlight = await updateFlight(req.params.id, req.body);
+        res.status(201);
+
+    } catch (err) {
+        console.log(err);
+        // if request fails...return 5OO
+        res.status(err?.status || 500).json(err);
+    }
+})
 
 app.all('*', (req,res) => {
     res.status(404).send("we dont have the route you\'re looking for");
